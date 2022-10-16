17e786465cd8e4bf4b33abcf1f42250e2715ef00
ImanolMM <imanolm.upv@gmail.com>
2022-10-16 11:17:37 +0200

    archivos

---
+++ src/OrderedCircularLinkedList.java
@@ -0,0 +1,14 @@
+public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+	
+	public void add(T elem){
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+
+
+
+
+
+
+	}
+
+
+}
