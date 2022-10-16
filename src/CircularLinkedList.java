1533b3908804c5c32535d1db8e93b4e7791d373f
ImanolMM <imanolm.upv@gmail.com>
2022-10-16 12:12:38 +0200

    avances

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -27,22 +27,61 @@ public class CircularLinkedList<T> implements ListADT<T> {
 	// Elimina el primer elemento de la lista
         // Precondici�n: la lista tiene al menos un elemento
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		T t= null;
+		if (this.last != null){
+			t=last.next.data;
+			this.last.next = this.last.next.next;
+		}
+		return t;
 	}
+	//coste: O(1)
 
 	public T removeLast() {
 	// Elimina el �ltimo elemento de la lista
         // Precondici�n: la lista tiene al menos un elemento
 			// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		Node act = this.last;
+		T t= null;
+		if (this.last != this.last.next){
+			while (act.next != this.last){
+				act = act.next;
+			}
+			t = this.last.data;
+			act.next = this.last.next;
+			this.last = act;
 
-		   }
+		} else {
+			t = this.last.data;
+			this.last = null;
+		}
+		return t;
+	}
 
 
 	public T remove(T elem) {
 	//Elimina un elemento concreto de la lista
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-	}
+		Node act= this.last;
+		Node ant= this.last;
+		boolean salir = false;
+		T t= null;
 
+		if (this.last != null){
+			act = act.next;
+
+			while (act != this.last && !salir){
+				if (act.data == elem){
+					t = (T) act.data;
+					ant.next = act.next;
+					salir = true;
+				}
+				ant = act;
+				act = act.next;
+			}
+		}
+		return t;
+	}
+	//coste: O (n)
 	public T first() {
 	//Da acceso al primer elemento de la lista
 	      if (isEmpty())
@@ -58,14 +97,32 @@ public class CircularLinkedList<T> implements ListADT<T> {
 	}
 
 	public boolean contains(T elem) {
+		boolean encontrado = false;
+		Node act = this.last;
+		if (this.last != null){
+			act = act.next;
+			while  (act != this.last){
+				if (act.data == elem){
+					encontrado = true;
+				}
+			}
+		}
+		return encontrado;
+	}
+	// coste: O (n)
 
-		   }
-
-	public T find(T elem) {
+	public T find(T elem) { //TODO preguntar si hay que devolver la posi o el elemento
 	//Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no est�
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		boolean encontrado = this.contains(elem);
+		if (encontrado){
+			return elem;
+		}
+		else{
+			return null;
+		}
 	}
+	//coste: O(n)
 
 	public boolean isEmpty() 
 	//Determina si la lista est� vac�a
---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    public static void main(String[] args){
+        CircularLinkedList t= new CircularLinkedList();
+        t.removeFirst();
+    }
+}
