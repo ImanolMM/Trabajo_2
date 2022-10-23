93626a80fb07918e234268fb9b70be5f32769c22
ImanolMM <imanolm.upv@gmail.com>
2022-10-23 12:06:03 +0200

    no avances

--- src/OrderedCircularLinkedList.java
+++ src/OrderedCircularLinkedList.java
@@ -1,14 +1,35 @@
-public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T>{
 	
-	public void add(T elem){
+	public void add(T elem){ //TODO preguntar si hay que hacer un CompareTo o si van a ser siempre numeros
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
-
-
-
-
-
+		Node act = last;
+		Node ant = last;
+		Node nuevo = new Node(elem);
+		boolean añadido = false;
+		if (last != null){
+			act = last.next;
+			while (act != last && !añadido){
+				if (elem.compareTo(act.data.toString()) < 0 ){
+					if (act == last.next){
+						nuevo.next = act;
+						last.next = nuevo;
+						last = nuevo;
+					}else{
+						nuevo.next = act;
+						ant.next = nuevo;
+					}
+					count++;
+					añadido = true;
+				}else{
+					ant = act;
+					act = act.next;
+				}
+			}
+		}else{
+			last= nuevo;
+			last.next = nuevo;
+			count ++;
+		}
 	}
-
 
 }
